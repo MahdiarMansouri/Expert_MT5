5f7cdeba9fa4cda8ba120f09f55a83e1e26bcd10
MahdiarMansouri <mahdiarm60@gmail.com>
2024-01-31 15:32:17 +0330

    defining strategy func

--- MT5.py
+++ MT5.py
@@ -1,4 +1,5 @@
 import MetaTrader5 as mt5
+from strategy import strategy
 import time
 
 # Configure your MT5 account details
@@ -22,6 +23,7 @@ if not account:
 account_info = mt5.account_info()
 if account_info is not None:
     print(f"Balance: {account_info.balance}, Equity: {account_info.equity}")
+    print('_' * 30)
 
 # prepare the buy request structure
 symbol = 'XAUUSD.ecn'
@@ -31,65 +33,80 @@ if symbol_info is None:
     print(symbol, "not found, can not call order_check()")
     mt5.shutdown()
     quit()
-# print(symbol_info)
+else:
+    print(f"Symbol: {symbol}")
+    print(f"Minimum volume: {symbol_info.volume_min}")
+    print(f"Maximum volume: {symbol_info.volume_max}")
+    print(f"Volume step: {symbol_info.volume_step}")
+    print('_' * 30)
+
 
 # if the symbol is unavailable in MarketWatch, add it
 if not symbol_info.visible:
-    print(symbol, "is not visible, trying to switch on")
+    print(symbol, "is not visible, trying to switch on symbol on your MarketWatch!")
     if not mt5.symbol_select(symbol, True):
-        print("symbol_select({}}) failed, exit", symbol)
+        print("symbol_select({}) failed, exit", symbol)
         mt5.shutdown()
         quit()
 
-lot = 0.01
+# Order params
+lot = 0.1
 point = mt5.symbol_info(symbol).point
 price = mt5.symbol_info_tick(symbol).ask
 deviation = 20
 
-# Define strategy for opening a trade
-def should_open_trade():
-    # Implement your trading strategy logic here
-    # For example, checking some indicators, market conditions, etc.
-    return True
+# Strategy params
+count = 10
+timeframe = mt5.TIMEFRAME_M1
 
-# Check if the strategy conditions are met
-if should_open_trade():
-    # Define request structure for trade
-    request = {
-        "action": mt5.TRADE_ACTION_DEAL,
-        "symbol": symbol,
-        "volume": lot,
-        "type": mt5.ORDER_TYPE_BUY,
-        "price": price,
-        "sl": price - 100 * point,  # Stop loss
-        "tp": price + 100 * point,  # Take profit
-        "deviation": deviation,
-        "magic": 234000,
-        "comment": "Python script trade",
-        "type_time": mt5.ORDER_TIME_GTC,
-        "type_filling": mt5.ORDER_FILLING_FOK,
-    }
+# Define strategy for sending order
+while True:
+    (comment, pin_bar_validation) = strategy(count, timeframe, symbol)
+    print(f'Comment: {comment} \nPinBar Validation: {pin_bar_validation}')
 
-    # send a trading request
-    result = mt5.order_send(request)
-    # check the execution result
-    print("1. order_send(): by {} {} lots at {} with deviation={} points".format(symbol, lot, price, deviation))
-    if result.retcode != mt5.TRADE_RETCODE_DONE:
-        print("2. order_send failed, retcode={}".format(result.retcode))
-        # request the result as a dictionary and display it element by element
-        result_dict = result._asdict()
-        for field in result_dict.keys():
-            print("   {}={}".format(field, result_dict[field]))
-            # if this is a trading request structure, display it element by element as well
-            if field == "request":
-                traderequest_dict = result_dict[field]._asdict()
-                for tradereq_filed in traderequest_dict:
-                    print("       traderequest: {}={}".format(tradereq_filed, traderequest_dict[tradereq_filed]))
-        print("shutdown() and quit")
-        mt5.shutdown()
-        quit()
+    # Check if the strategy conditions are Ture
+    if pin_bar_validation:
+        # Define request structure for trade
+        request = {
+            "action": mt5.TRADE_ACTION_DEAL,
+            "symbol": symbol,
+            "volume": lot,
+            "type": mt5.ORDER_TYPE_BUY,
+            "price": price,
+            "sl": price - 100 * point,  # Stop loss
+            "tp": price + 100 * point,  # Take profit
+            "deviation": deviation,
+            "magic": 234000,
+            "comment": "Python script trade",
+            "type_time": mt5.ORDER_TIME_GTC,
+            "type_filling": mt5.ORDER_FILLING_FOK,
+        }
+
+        # send a trading request
+        result = mt5.order_send(request)
+        # check the execution result
+        print("1. order_send(): by {} {} lots at {} with deviation={} points".format(symbol, lot, price, deviation))
+        if result.retcode != mt5.TRADE_RETCODE_DONE:
+            print("2. order_send failed, retcode={}".format(result.retcode))
+            # request the result as a dictionary and display it element by element
+            result_dict = result._asdict()
+            for field in result_dict.keys():
+                print("   {}={}".format(field, result_dict[field]))
+                # if this is a trading request structure, display it element by element as well
+                if field == "request":
+                    traderequest_dict = result_dict[field]._asdict()
+                    for tradereq_filed in traderequest_dict:
+                        print("       traderequest: {}={}".format(tradereq_filed, traderequest_dict[tradereq_filed]))
+            print("shutdown() and quit")
+            mt5.shutdown()
+            quit()
+        else:
+            print("Order successfully placed!", end='\n\n')
+            print('=' * 20)
     else:
-        print("Order successfully placed!")
+        print('-' * 10)
+
+    time.sleep(60)
 
 # Shutdown the MT5 connection
 mt5.shutdown()
--- strategy.py
+++ strategy.py
@@ -14,18 +14,20 @@ symbol = "XAUUSD.ecn"
 timeframe = mt5.TIMEFRAME_M1  # 1-minute timeframe
 count = 10
 
-while True:
+
+def strategy(count, timeframe, symbol):
     # finding last closed bar
     last_2_bars = mt5.copy_rates_from_pos(symbol, timeframe, 0, 2)
-    last_closed_bar = last_2_bars[0]
-
+    print(last_2_bars)
+    last_closed_bar = last_2_bars[-1]
     # Defining direction of bar
-    candle_color = 'green' if last_closed_bar['close'] - last_closed_bar['open'] > 0 else 'red'
+    last_candle_color = 'green' if last_closed_bar['close'] - last_closed_bar['open'] > 0 else 'red'
+    print(f'Last Closed bar: {last_closed_bar} \n and last closed bar color {last_candle_color}')
 
     # Defining values of bar parts (body, shadow up, shadow down)
     body = np.abs(last_closed_bar['close'] - last_closed_bar['open'])
 
-    if candle_color == 'green':
+    if last_candle_color == 'green':
         shadow_up = last_closed_bar['high'] - last_closed_bar['close']
         shadow_down = last_closed_bar['open'] - last_closed_bar['low']
     else:
@@ -33,38 +35,38 @@ while True:
         shadow_down = last_closed_bar['close'] - last_closed_bar['low']
 
     # Defining pin bar
-    pin_bar = None
+    pin_bar = False
 
     if shadow_up or shadow_down > body:
         pin_bar = last_closed_bar
 
     if pin_bar:
-        print('yes')
+        # Request the last n 1-minute bars
+        bars = mt5.copy_rates_from_pos(symbol, timeframe, 0, count)
 
-    exit()
+        # Display the bars
+        for bar in bars:
+            # Convert time in seconds to the datetime format
+            time_ = datetime.fromtimestamp(bar['time'])
+            candle_color = 'green' if bar['close'] - bar['open'] > 0 else 'red'
+            print(
+                f"Time: {time_}, Open: {bar['open']}, High: {bar['high']}, Low: {bar['low']}, Close: {bar['close']}, Color: {candle_color}")
 
-    # Request the last 2 1-minute bars
-    bars = mt5.copy_rates_from_pos(symbol, timeframe, 0, count)
-    # print(bars['close'])
-    # print(type(bars))
-
-    # Display the bars
-    for bar in bars:
-        # Convert time in seconds to the datetime format
-        time_ = datetime.fromtimestamp(bar['time'])
-        candle_color = 'green' if bar['close'] - bar['open'] > 0 else 'red'
-        print(
-            f"Time: {time_}, Open: {bar['open']}, High: {bar['high']}, Low: {bar['low']}, Close: {bar['close']}, Color: {candle_color}")
-
-    candles_close_mean = np.mean(bars['close'])
-    if candles_close_mean > bars['close'][-1]:
-        trend = 'down'
+        # Defining last n candles trend
+        candles_close_mean = np.mean(bars['close'])
+        if candles_close_mean > bars['close'][-1]:
+            trend = 'down'
+            # Check the valid pin bar for trend
+            if (shadow_down / 3) > shadow_up:
+                return True
+            else:
+                return False
+        else:
+            trend = 'up'
+            if (shadow_up / 3) > shadow_down:
+                return True
+            else:
+                return False
     else:
-        trend = 'up'
-
-
-
-    print(trend)
-    print('-' * 50)
-    time.sleep(60)
-
+        comment = 'Its not PinBar...'
+        return comment, False
