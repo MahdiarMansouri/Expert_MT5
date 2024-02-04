27fec9d0820f901fd9f1201609a095db509a95c4
Mahdiar <mahdiarm60@gmail.com>
2024-02-04 05:52:02 -0800

    complete orders class for sell and buy in True strategy situation

--- MT5.py
+++ MT5.py
@@ -1,5 +1,6 @@
 import MetaTrader5 as mt5
 from strategy import StrategyValidator
+from orders import SetOrders
 import time
 
 # Configure your MT5 account details
@@ -74,53 +75,15 @@ while True:
         validator_comment, validation = SV.pinbar_validator()
         print(f'Comment: {validator_comment}, \nPinBar Validation: {validation}')
         if validation ==1:
+            set_order = SetOrders(symbol, lot, timeframe)
             # check trend for order type (buy or sell)
             if trend == 'down':
-                order_type = mt5.ORDER_TYPE_BUY
-                tp = price + 100 * point
-                sl = price - 100 * point
+                result = set_order.set_buy_order()
+                order_situation = set_order.check_order(result)
             else:
-                order_type = mt5.ORDER_TYPE_SELL
-                tp = price - 100 * point
-                sl = price + 100 * point
-
-            # Define request structure for trade
-            request = {
-                "action": action,
-                "symbol": symbol,
-                "volume": lot,
-                "type": order_type,
-                "price": price,
-                "sl": sl,  # Stop loss
-                "tp": tp,  # Take profit
-                "deviation": deviation,
-                "magic": 234000,
-                "comment": "Python script trade",
-                "type_time": mt5.ORDER_TIME_GTC,
-                "type_filling": mt5.ORDER_FILLING_FOK,
-            }
-
-            # send a trading request
-            result = mt5.order_send(request)
-            # check the execution result
-            print("1. order_send(): by {} {} lots at {} with deviation={} points".format(symbol, lot, price, deviation))
-            if result.retcode != mt5.TRADE_RETCODE_DONE:
-                print("2. order_send failed, retcode={}".format(result.retcode))
-                # request the result as a dictionary and display it element by element
-                result_dict = result._asdict()
-                for field in result_dict.keys():
-                    print("   {}={}".format(field, result_dict[field]))
-                    # if this is a trading request structure, display it element by element as well
-                    if field == "request":
-                        traderequest_dict = result_dict[field]._asdict()
-                        for tradereq_filed in traderequest_dict:
-                            print("       traderequest: {}={}".format(tradereq_filed, traderequest_dict[tradereq_filed]))
-                print("shutdown() and quit")
-                mt5.shutdown()
-                quit()
-            else:
-                print("Order successfully placed!", end='\n\n')
-                print('=' * 20)
+                result = set_order.set_sell_order()
+                order_situation = set_order.check_order(result)
+            print(f'Order situation: {order_situation}')
         else:
             print('-' * 10)
 
--- orders.py
+++ orders.py
@@ -40,18 +40,47 @@ class SetOrders:
         self.request['sl'] = sl
         self.request['tp'] = tp
 
-        # Define request structure for trade
-        request = {
-            "action": self.action,
-            "symbol": self.symbol,
-            "volume": self.lot,
-            "type": order_type,
-            "price": self.buy_price,
-            "sl": sl,
-            "tp": tp,
-            "deviation": self.deviation,
-            "magic": self.magic,
-            "comment": "Python Expert Trade",
-            "type_time": self.type_time,
-            "type_filling": self.type_filling,
-        }
+        # send a trading request
+        order_result = mt5.order_send(self.request)
+
+        # check the execution result
+        print("1. order_send(): by {} {} lots at {} with deviation={} points".format(self.symbol, self.lot,
+                                                                                     self.buy_price, self.deviation))
+        return order_result
+
+    def set_sell_order(self):
+        order_type = mt5.ORDER_TYPE_SELL
+        tp = self.sell_price - 100 * self.point
+        sl = self.sell_price + 100 * self.point
+        self.request['type'] = order_type
+        self.request['price'] = self.buy_price
+        self.request['sl'] = sl
+        self.request['tp'] = tp
+
+        # send a trading request
+        order_result = mt5.order_send(self.request)
+
+        # check the execution result
+        print("1. order_send(): by {} {} lots at {} with deviation={} points".format(self.symbol, self.lot,
+                                                                                     self.sell_price, self.deviation))
+        return order_result
+
+    def check_order(self, result):
+        if result.retcode != mt5.TRADE_RETCODE_DONE:
+            print("2. order_send failed, retcode={}".format(result.retcode))
+            # request the result as a dictionary and display it element by element
+            result_dict = result._asdict()
+            for field in result_dict.keys():
+                print("   {}={}".format(field, result_dict[field]))
+                # if this is a trading request structure, display it element by element as well
+                if field == "request":
+                    traderequest_dict = result_dict[field]._asdict()
+                    for tradereq_filed in traderequest_dict:
+                        print("       traderequest: {}={}".format(tradereq_filed, traderequest_dict[tradereq_filed]))
+            print("shutdown() and quit")
+            mt5.shutdown()
+            return False
+        else:
+            print("Order successfully placed!", end='\n\n')
+            print('=' * 20)
+            return True
