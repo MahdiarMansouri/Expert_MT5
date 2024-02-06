f26aa0a02af7f2198b2fbc696bb8c9b262e3467a
MahdiarMansouri <mahdiarm60@gmail.com>
2024-02-06 19:08:53 +0330

    debug tp and sl

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import MetaTrader5 as mt5
+from strategy import StrategyValidator
+from orders import SetOrders
 import time
 
 # Configure your MT5 account details
 account_number = 516701  # Replace with your account number
-password = 'p&X7pbw#AFyK'           # Replace with your password
-server = 'OtetGroup-MT5'               # Replace with your server
-path = 'C:\Program Files\MetaTrader 5\terminal64.exe'
+password = 'p&X7pbw#AFyK'  # Replace with your password
+server = 'OtetGroup-MT5'  # Replace with your server
+path = 'C:\Program Files\MetaTrader 5\\terminal64.exe'
+
 # Initialize MT5 connection
 account = mt5.initialize(path=path,
                          login=account_number,
@@ -13,7 +16,7 @@ account = mt5.initialize(path=path,
                          server=server,
                          portable=False)
 
-if not mt5.initialize(login=account_number, server=server, password=password):
+if not account:
     print("initialize() failed, error code =", mt5.last_error())
     quit()
 
@@ -21,55 +24,45 @@ if not mt5.initialize(login=account_number, server=server, password=password):
 account_info = mt5.account_info()
 if account_info is not None:
     print(f"Balance: {account_info.balance}, Equity: {account_info.equity}")
+    print('_' * 30)
 
 # prepare the buy request structure
-symbol = 'EURUSD'
+symbol = 'XAUUSD.ecn'
 
 symbol_info = mt5.symbol_info(symbol)
 if symbol_info is None:
     print(symbol, "not found, can not call order_check()")
     mt5.shutdown()
     quit()
+else:
+    print(f"Symbol: {symbol}")
+    print(f"Minimum volume: {symbol_info.volume_min}")
+    print(f"Maximum volume: {symbol_info.volume_max}")
+    print(f"Volume step: {symbol_info.volume_step}")
+    print('_' * 30)
 
-# Define trading parameters
-symbol = 'EURUSD'
-lot = 0.01
+# if the symbol is unavailable in MarketWatch, add it
+if not symbol_info.visible:
+    print(symbol, "is not visible, trying to switch on symbol on your MarketWatch!")
+    if not mt5.symbol_select(symbol, True):
+        print("symbol_select({}) failed, exit", symbol)
+        mt5.shutdown()
+        quit()
 
-# Define strategy for opening a trade
-def should_open_trade():
-    # Implement your trading strategy logic here
-    # For example, checking some indicators, market conditions, etc.
-    return True
+# Order params
+lot = 0.1
 
-# Main trading loop
-while True:
-    # Check if the strategy conditions are met
-    if should_open_trade():
-        # Define request structure for trade
-        request = {
-            "action": mt5.TRADE_ACTION_DEAL,
-            "symbol": symbol,
-            "volume": lot,
-            "type": mt5.ORDER_TYPE_BUY,
-            "price": mt5.symbol_info_tick(symbol),
-            "sl": 0,  # Stop loss
-            "tp": 0,  # Take profit
-            "deviation": 20,
-            "magic": 234000,
-            "comment": "Python script trade",
-            "type_time": mt5.ORDER_TIME_GTC,
-            "type_filling": mt5.ORDER_FILLING_IOC,
-        }
+# Strategy params
+last_n_bars = 10
+timeframe = mt5.TIMEFRAME_M1
 
-        # Send trade request
-        result = mt5.order_send(request)
-        if result != mt5.TRADE_RETCODE_DONE:
-            print("Failed to send order :(", result)
-        else:
-            print("Order successfully placed!")
 
-    # Sleep for a while before the next iteration
-    time.sleep(60)  # Sleep for 60 seconds
+vbar = mt5.copy_rates_from_pos(symbol, timeframe, 1, 1)
+pbar = mt5.copy_rates_from_pos(symbol, timeframe, 2, 1)
+st = SetOrders(vbar, pbar, symbol, lot, timeframe)
+r = st.set_buy_order()
+# print(r)
+print(st.check_order(r))
 
 # Shutdown the MT5 connection
 mt5.shutdown()
--- orders.py
+++ orders.py
@@ -12,6 +12,9 @@ class SetOrders:
         self.buy_price = mt5.symbol_info(symbol).ask
         self.sell_price = mt5.symbol_info(symbol).bid
         self.point = mt5.symbol_info(symbol).point
+        print('point is => ', self.point)
+        self.spread = mt5.symbol_info(symbol).spread
+        print('spread is => ', self.spread)
         self.action = mt5.TRADE_ACTION_DEAL
         self.type_time = mt5.ORDER_TIME_GTC
         self.type_filling = mt5.ORDER_FILLING_FOK
@@ -36,9 +39,13 @@ class SetOrders:
 
     def set_buy_order(self):
         order_type = mt5.ORDER_TYPE_BUY
-        step = (self.validator_bar['close'] - self.pin_bar['low']) + (1.5 * (np.abs(self.buy_price - self.sell_price)))
+        e = self.validator_bar['close'] - self.pin_bar['low']
+        step = e[0] + (self.spread * self.point)
+        print(f'step => {step}')
         tp = self.buy_price + step
+        print(f'tp => {tp}')
         sl = self.buy_price - step
+        print(f'sl => {sl}')
         self.request['type'] = order_type
         self.request['price'] = self.buy_price
         self.request['sl'] = sl
@@ -54,9 +61,13 @@ class SetOrders:
 
     def set_sell_order(self):
         order_type = mt5.ORDER_TYPE_SELL
-        step = (self.pin_bar['high'] - self.validator_bar['close']) + (1.5 * (np.abs(self.buy_price - self.sell_price)))
-        tp = self.sell_price - step
-        sl = self.sell_price + step
+        e = self.pin_bar['high'] - self.validator_bar['close']
+        step = e[0] + (self.spread * self.point)
+        print(f'step => {step[0]}')
+        tp = self.sell_price - step[0]
+        print(f'tp => {tp}')
+        sl = self.sell_price + 2 * step[0]
+        print(f'sl => {sl}')
         self.request['type'] = order_type
         self.request['price'] = self.buy_price
         self.request['sl'] = sl
@@ -71,21 +82,22 @@ class SetOrders:
         return order_result
 
     def check_order(self, result):
-        if result.retcode != mt5.TRADE_RETCODE_DONE:
-            print("2. order_send failed, retcode={}".format(result.retcode))
-            # request the result as a dictionary and display it element by element
-            result_dict = result._asdict()
-            for field in result_dict.keys():
-                print("   {}={}".format(field, result_dict[field]))
-                # if this is a trading request structure, display it element by element as well
-                if field == "request":
-                    traderequest_dict = result_dict[field]._asdict()
-                    for tradereq_filed in traderequest_dict:
-                        print("       traderequest: {}={}".format(tradereq_filed, traderequest_dict[tradereq_filed]))
-            print("shutdown() and quit")
-            mt5.shutdown()
-            return False
-        else:
-            print("Order successfully placed!", end='\n\n')
-            print('=' * 20)
-            return True
+        if result.retcode:
+            if result.retcode != mt5.TRADE_RETCODE_DONE:
+                print("2. order_send failed, retcode={}".format(result.retcode))
+                # request the result as a dictionary and display it element by element
+                result_dict = result._asdict()
+                for field in result_dict.keys():
+                    print("   {}={}".format(field, result_dict[field]))
+                    # if this is a trading request structure, display it element by element as well
+                    if field == "request":
+                        traderequest_dict = result_dict[field]._asdict()
+                        for tradereq_filed in traderequest_dict:
+                            print("       traderequest: {}={}".format(tradereq_filed, traderequest_dict[tradereq_filed]))
+                print("shutdown() and quit")
+                mt5.shutdown()
+                return False
+            else:
+                print("Order successfully placed!", end='\n\n')
+                print('=' * 20)
+                return True
